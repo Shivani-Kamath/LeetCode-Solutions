bc7e1c82929e3f6898cb7e8f6a8ab374e2a417f3
shivani kamath <shivanikamath369@gmail.com>
2025-05-10 19:51:40 +0530

    Solution for LeetCode 35: Search Insert Position

--- RemoveDuplicatesFromSortedArray.java
+++ RemoveDuplicatesFromSortedArray.java
@@ -1,10 +1,12 @@
-int removeDuplicates(int* nums, int numsSize) {
+class Solution {
+    public int removeDuplicates(int[] nums) {
     int left,right=0,count=0;
-    for(left=1;left<numsSize;left++){
+    for(left=1;left<nums.length;left++){
         if(nums[left]!=nums[right]){
         right++;
         nums[right]=nums[left];
         }
     }
     return right+1;
+    }
 }
